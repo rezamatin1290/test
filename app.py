e91b9987a98f69c2a9fcdce9d49be7fa94c19b4d
spyreza <rmatin65@yahoo.com>
2019-10-16 09:33:28 +0330

    add Empty choice for choices

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ text = input(wmsg+"text: "+rmsg+": ")
 
 confirmation = input("\nsubject: {}\nmatn:{}\nAya Mayel Hastid Sabt Shavad[Y/n]? ".format(subject, text))
 
-if confirmation.casefold() == "y" or confirmation.casefold() == "yes":
+if confirmation.casefold() == "y" or confirmation.casefold() == "yes" or confirmation == "":
     l = Curl(subject, text)
     l.throw_to_db()
 
