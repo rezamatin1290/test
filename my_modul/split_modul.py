a894641cda3d90ff798601cf9c5eb5bcd30a23bb
rezamatin1290 <55682278+rezamatin1290@users.noreply.github.com>
2019-11-02 11:38:30 +0330

    Update split_modul.py

--- my_modul/split_modul.py
+++ my_modul/split_modul.py
@@ -1,11 +1,14 @@
-def split(string,spliter):
-    list1=[];temp=""
+def split(string, spliter):
+    list1=[]
+    temp=""
     for x in range(len(string)):
-        if string[x]!=spliter:
-            temp=temp+string[x]
-        elif string[x]==spliter:
+        if string[x] != spliter:
+            temp = temp + string[x]
+        elif string[x] == spliter:
             if (temp not in list1):
-                list1.append(temp);temp=""
+                list1.append(temp)
+                temp=""
     if len(temp)>0:
-        list1.append(temp);temp=""
+        list1.append(temp)
+        temp=""
     return list1
