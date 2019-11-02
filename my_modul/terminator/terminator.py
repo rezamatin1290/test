39f1b30cf63c9c163c63dd0a77feeaa892e2a9b6
rezamatin1290 <55682278+rezamatin1290@users.noreply.github.com>
2019-11-02 16:42:39 +0330

    Update terminator.py

--- my_modul/terminator/terminator.py
+++ my_modul/terminator/terminator.py
@@ -4,8 +4,8 @@ for x in range(10000):
 
 for x in  range(len(s)):
 	for y in range(200):
-		d="spy_dir"+str(x)+str(y)
-		s[x]=open("/home/{0}.txt".format(d)","w")
+		d = "spy_dir" + str(x) + str(y)
+		s[x] = open("/home/{0}.txt".format(d)","w")
 		for z in range(1000):
-			s[x].write(str(z)+"/n")
+			s[x].write(str(z) + "/n")
 
