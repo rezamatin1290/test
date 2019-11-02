74fca76f1bbac7eabcdb51c7d82500370a04edbf
rezamatin1290 <55682278+rezamatin1290@users.noreply.github.com>
2019-11-02 16:45:57 +0330

    Update linklist.py

--- my_modul/linklist.py
+++ my_modul/linklist.py
@@ -2,18 +2,18 @@ class Reza(object):
     first=""
     last=""
     def __init__(self,name):
-        if Reza.first=="" and Reza.last=="":
-            Reza.last=Reza.first=self;
-            self.name=name
+        if Reza.first == "" and Reza.last == "":
+            Reza.last = Reza.first = self;
+            self.name = name
         else:
-            Reza.last.next=self
-            Reza.last=self
-            self.name=name
+            Reza.last.next = self
+            Reza.last = self
+            self.name = name
     def pirint(self):
         try:
-            temp=Reza.first
+            temp = Reza.first
             while (True):
                 print(temp.name)
-                temp=temp.next
+                temp = temp.next
         except:
             pass
