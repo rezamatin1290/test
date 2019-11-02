187cf26a7fcad1ed4ee43b95d88eb3076ffb16cf
rezamatin1290 <55682278+rezamatin1290@users.noreply.github.com>
2019-11-02 16:48:37 +0330

    Update namad_almi.py

--- my_modul/namad_almi.py
+++ my_modul/namad_almi.py
@@ -1,20 +1,21 @@
 def bend(a):
     for x in range(len(a)):
-        if a[x]!="0" and a[x]!=".":
+        if a[x] != "0" and a[x] != ".":
             print(a[x]+"."+a[x+1:],"(10**-",x-1,")",sep="")
             break
 while(True):
     try:
-        a=input("Enter: ")
-        a=float(a)
+        a = input("Enter: ")
+        a = float(a)
     except:
         print("enter a number:")
     else:
-        a=str(a);
+        a = str(a);
         if "e" in a:
             print(a[:str(a).index("e")]+"(*10**-"+a[str(a).index("e")+2:]+")");break
-        if a[0]=="0":
-            bend(a);break
+        if a[0] == "0":
+            bend(a)
+            break
         b=a.index(".")
         if b!=1:
             a=a[:b]+a[b+1:]
