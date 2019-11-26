6397579fd77446740b335aa54243499ab9eca013
rezamatin1290 <55682278+rezamatin1290@users.noreply.github.com>
2019-11-26 16:31:09 +0330

    Update process.py

--- process.py
+++ process.py
@@ -1,9 +1,11 @@
 
 a_time = [] # arrival time
 s_time = [] # service time
+p_name = []
 finish_time = []
 
 for x in range(int( input("تعداد پردازش : ") )):
+    p_name.append(input("procss name : "))
     a_time.append( int(input("arrival time : ")) )
     s_time.append( int(input("service time : ")) )
     if len(finish_time) == 0:
@@ -16,8 +18,10 @@ Tr_time = [finish_time[x] - a_time[x] for x in range(len(a_time))]
 
 def p_f(text, l):
     print(text,end = "  ")
+    c = 0
     for x in l:
-        print(x, end = "  ")
+        print(f"procss {p_name[c]} => ", x, end = "  ")
+        c +=1
 
 p_f("Finish time : ", finish_time)
 print()
