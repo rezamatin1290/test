43a340c2289b63f190f896ffb140a3ca630ba552
rezamatin1290 <55682278+rezamatin1290@users.noreply.github.com>
2019-12-11 11:20:58 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from sys import stdout
 
 from time import sleep
-
 class star:
     BLUE = '{}'.format('\033[94m')
     GREEN = '{}'.format('\033[92m')
@@ -9,7 +8,13 @@ class star:
     YELLOW = '{}'.format('\033[33m')
     WHITE = "{}".format("\033[0m")
 
+    
+    
 def show_out(matn, s):
+    """
+    show and clear out put
+    
+    """
     stdout.write(f"{star.BLUE}{str(matn)}")
     stdout.write("\033[?25h")
     stdout.flush()
