904c640751d7f3cb0f3bdd4a36a6aa15b5602c4c
rezamatin1290 <55682278+rezamatin1290@users.noreply.github.com>
2019-12-11 11:19:48 +0330

    add test.py for show and clear screen

---
+++ test.py
@@ -0,0 +1,23 @@
+from sys import stdout
+
+from time import sleep
+
+class star:
+    BLUE = '{}'.format('\033[94m')
+    GREEN = '{}'.format('\033[92m')
+    RED = '{}'.format('\033[91m')
+    YELLOW = '{}'.format('\033[33m')
+    WHITE = "{}".format("\033[0m")
+
+def show_out(matn, s):
+    stdout.write(f"{star.BLUE}{str(matn)}")
+    stdout.write("\033[?25h")
+    stdout.flush()
+    sleep(s)
+    stdout.write("\b"*len(str(matn)))
+    stdout.write("\033[?25h")
+    stdout.flush()
+
+
+show_out("reza", 0.56)
+
