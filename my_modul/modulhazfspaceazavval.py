67b6374991f504083ef7b6287172174d9358bd9c
rezamatin1290 <55682278+rezamatin1290@users.noreply.github.com>
2022-11-01 17:03:44 +0330

    Delete my_modul directory

--- my_modul/modulhazfspaceazavval.py
+++
@@ -1,9 +0,0 @@
-#این اسکریپت فایلها رو میخونه و هر سط رو بدون اسپیس در لیست میریزه  
-
-f=open("/home/spyreza/Desktop/nwe.txt");l=[]
-#print(f.read(),end="")
-counter=1
-for line in f:
-    l.append("row {0}  is {1}".format(counter,line.strip()))
-    counter+=1
-print(l)
